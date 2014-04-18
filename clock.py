6a76117f06ef3b71ff5c6857aae7c1051ce876c9
SarahLKelley <sarahlkelley@gmail.com>
2014-04-18 05:39:59 -0400

    additions and modifications for DDL

--- clock.py
+++ clock.py
@@ -4,6 +4,8 @@
 #
 # Author:   Benjamin Bengfort <benjamin@bengfort.com>
 # Created:  Sat Mar 08 15:29:47 2014 -0500
+# Author:   Sarah Kelley <sarahlkelley@gmail.com.com>
+# Edited:   Fri Apr 18 00:57:42 EDT 2014
 #
 # Copyright (C) 2014 Bengfort.com
 # For license information, see LICENSE.txt
@@ -30,19 +32,18 @@ class Clock(object):
 
     # The "default" formats. Add more formats via subclasses or in the
     # instantation of a Clock object (or just add more here).
+
+    # These formats are more complicated string formatting
     FORMATS = {
         "code":"%a %b %d %H:%M:%S %Y %z",
         "json":"%Y-%m-%dT%H:%M:%S.%fZ",
         "cute":"%b %d, %Y",
     }
 
+    # class method - bound to the class
     @classmethod
     def local_now(klass):
         return datetime.now(tzlocal())
-
-    @classmethod
-    def utc_now(klass):
-        return datetime.utcnow()
 
     def __init__(self, formats={}):
         self.formats = self.FORMATS.copy()
@@ -51,19 +52,11 @@ class Clock(object):
     def _local_format(self, fmt):
         return Clock.local_now().strftime(fmt)
 
-    def _utc_format(self, fmt):
-        return Clock.utc_now().strftime(fmt)
-
     def get_stamp(self, name):
+        # strips "-" from beginning and end of string
+        # the string is "-code", "-json" or "-cute"
         name  = name.strip("-")
-        mname = name + "_stamp"
 
-        # Try to find method table first
-        if hasattr(self, mname):
-            method = getattr(self, mname)
-            return method()
-
-        # Try to use the format string with local timezone
         if name in self.formats:
             return self._local_format(self.formats[name])
 
@@ -76,39 +69,11 @@ class Clock(object):
             else:
                 print "No stamp format for name %s" % name
 
-    def help_stamp(self):
-
-        output = ["Prints a timestamp represented with a format.",
-                  "",
-                  "The formats are stored in a lookup table with names, that",
-                  "you can pass to the function. For instance, if you pass the",
-                  "following arguments, you'll get the following results:",
-                  ""]
-
-        for name in ('--code', '--json'):
-            output.append("\t%s: %s" % (name, self.get_stamp(name)))
-
-        output.append("")
-        output.append("The current formats are:")
-        output.append("")
-
-        for item in self.formats.items():
-            output.append("\t%s: \"%s\"" % item)
-        output.append("")
-
-        output.append("Note that the timezone will default to the system timezone unless")
-        output.append("The format requires a UTC or other timezone (like JSON)")
-        output.append("")
-
-        return "\n".join(output)
-
-    def json_stamp(self):
-        return self._utc_format(self.formats['json'])
-
 ##########################################################################
 ## Main Method, handle inputs to program from command line
 ##########################################################################
 
+# print __name__ in the python shell to see how this works
 if __name__ == "__main__":
 
     args  = sys.argv[1:]
